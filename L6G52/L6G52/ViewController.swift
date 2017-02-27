d52d3e8ff13bddff5ce688b49fbc0d5cfa7ad4aa
Slava Utyuzh <slavautyuzh@mac-mini-slava.nash.net.ua>
2017-02-27 15:20:53 +0200

    Added viewController LifeCircle

--- L6G52/L6G52/ViewController.swift
+++ L6G52/L6G52/ViewController.swift
@@ -9,14 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        print(#function)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print(#function)
+
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print(#function)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#function)
+    
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#function)
+
+    }
+ 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#function)
+
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        print(#function)
         // Dispose of any resources that can be recreated.
     }
 
