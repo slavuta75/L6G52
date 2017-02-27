7d45892037fb997e9a3bb003d32d7712069f027a
Slava Utyuzh <slavautyuzh@mac-mini-slava.nash.net.ua>
2017-02-27 14:46:07 +0200

    Initalize

---
+++ L6G52/L6G52/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L6G52
+//
+//  Created by Slava Utyuzh on 27.02.17.
+//  Copyright © 2017 Slava Utyuzh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
